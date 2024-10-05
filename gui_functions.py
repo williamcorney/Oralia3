fc02e3d10f09b4150a48862a09008041fb324789
williamcorney <118494245+williamcorney@users.noreply.github.com>
2024-10-06 00:01:40 +0100

    1

---
+++ gui_functions.py
@@ -0,0 +1,9 @@
+from gui_setup import MainWindow
+from PyQt6.QtWidgets import QListWidgetItem
+from PyQt6.QtGui import QPixmap, QFont
+from PyQt6.QtWidgets import QTabWidget
+class gui_functions (MainWindow):
+    addTab(self.settings_tab, "")
+
+    def note_handler(self,message):
+        pass
---
+++ gui_setup.py
@@ -0,0 +1,8 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QListWidget, QListWidgetItem,QAbstractItemView,QSpinBox
+from PyQt6.QtGui import QPixmap, QFont
+from PyQt6.QtWidgets import QTabWidget
+
+class MainWindow(QMainWindow):
+    pass
